2729bed2ffa19a3927dbc597263ba3a63f3ca82e
RickaPrincy <rckprincy@gmail.com>
2024-04-02 23:21:10 +0300

    feat: range statements

--- src/operations/statements/list.tsx
+++ src/operations/statements/list.tsx
@@ -6,9 +6,48 @@ import { accountProvider } from "@/providers/account-provider";
 import { renderMoney } from "@/utils/money";
 import { formatDate } from "@/utils/date";
 import Link from "next/link";
-import { Button } from "@chakra-ui/react";
+import { Box, Button, Input, Text } from "@chakra-ui/react";
+import { ChangeEvent, useState } from "react";
+
+function SelectDate(props: any) {
+  const { from, to, update } = props;
+  return (
+    <Box sx={{ my: 5, display: "flex", gap: 2, flexDirection: "column" }}>
+      <Text>From</Text>
+      <Input
+        type="datetime-local"
+        name="from"
+        placeholder="From"
+        value={from}
+        onChange={update}
+      />
+      <Text>To</Text>
+      <Input
+        type="datetime-local"
+        name="to"
+        placeholder="to"
+        value={to}
+        onChange={update}
+      />
+    </Box>
+  );
+}
 
 export function StatementList({ accountId }: { accountId: string }) {
+  const [range, setRange] = useState({
+    from: "2000-01-01",
+    to: "2025-01-01",
+  });
+
+  const update = (event: ChangeEvent<HTMLInputElement>) => {
+    setRange((prev) => {
+      return {
+        ...prev,
+        [event.target.name]: event.target.value,
+      };
+    });
+  };
+
   const labels: LabelType<Statement>[] = [
     { source: "ref", label: "Ref", size: "20%" },
     {
@@ -37,11 +76,12 @@ export function StatementList({ accountId }: { accountId: string }) {
     <List
       labels={labels}
       source={["statements"]}
-      keys={[]}
       provider={() =>
-        accountProvider.getBankStatements(accountId, "2020-01-01", "2025-01-01")
+        accountProvider.getBankStatements(accountId, range.from, range.to)
       }
+      keys={[range.from, range.to]}
       overviewProps={{
+        content: <SelectDate update={update} {...range} />,
         leftButton: (
           <Link href={`/accounts/${accountId}`}>
             <Button size="sm" px={5} colorScheme="blue">
